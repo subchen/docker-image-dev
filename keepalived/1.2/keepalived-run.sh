f29532ee2a8117cc73007065c76d66dd1439f295
Guoqiang Chen <subchen@gmail.com>
2015-12-24 10:37:50 +0800

    Update keepalived

--- keepalived/1.2/keepalived-run.sh
+++ keepalived/1.2/keepalived-run.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-KEEPALIVED_CFG=/etc/keepalived/keepalived.cfg
+KEEPALIVED_CONF=/etc/keepalived/keepalived.conf
 
-if [ ! -f $KEEPALIVED_CFG ]; then
+if [ ! -f $KEEPALIVED_CONF ]; then
   state=${KEEPALIVED_STATE:-MASTER}
-  priority=${KEEPALIVED_PRIORITY:-999}
+  priority=${KEEPALIVED_PRIORITY:-200}
+  src=$(ifconfig eth0 | sed -n -r 's/.*inet ([0-9\.]+).*/\1/p')
   peer=${KEEPALIVED_PEER}
   vip=${KEEPALIVED_VIP:-172.17.0.200}
 
-  # master is 999, backup is 100
-  if [[ "$state" == "BACKUP" && "$priority" == ""  ]]; then
+  # master is 200, backup is 100
+  if [[ "$state" == "BACKUP" && "$priority" == "" ]]; then
     priority=100
   fi
 
   sed -e "s/{{state}}/$state/g" \
       -e "s/{{priority}}/$priority/g" \
+      -e "s/{{src}}/$src/g" \
       -e "s/{{peer}}/$peer/g" \
       -e "s/{{vip}}/$vip/g" \
-      $KEEPALIVED_CFG.in > $KEEPALIVED_CFG
+      $KEEPALIVED_CONF.in > $KEEPALIVED_CONF
 fi
 
-exec /usr/sbin/keepalived -f $KEEPALIVED_CFG --dont-fork --log-console
+exec /usr/sbin/keepalived -f $KEEPALIVED_CONF --dont-fork --log-console
 
