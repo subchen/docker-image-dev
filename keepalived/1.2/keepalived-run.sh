a81dd6c6473094948547529e0e7ff6ffdf1372c1
Sub <subchen@gmail.com>
2016-02-25 12:53:46 +0800

    Update keepalived-run.sh

--- keepalived/1.2/keepalived-run.sh
+++ keepalived/1.2/keepalived-run.sh
@@ -4,23 +4,27 @@ KEEPALIVED_CONF=/etc/keepalived/keepalived.conf
 
 if [ ! -f $KEEPALIVED_CONF ]; then
   state=${KEEPALIVED_STATE:-MASTER}
-  priority=${KEEPALIVED_PRIORITY:-200}
-  src=$(ip addr show eth0 | sed -n -r 's/.*inet ([0-9.]+).*/\1/p')
   peer=${KEEPALIVED_PEER}
   vip=${KEEPALIVED_VIP:-172.17.0.200}
 
-  # master is 200, backup is 100
-  if [[ "$KEEPALIVED_STATE" = "BACKUP" && "$KEEPALIVED_PRIORITY" = "" ]]; then
+  # master is 150, backup is 100
+  if [[ "$KEEPALIVED_STATE" = "MASTER" ]]; then
+    state=BACKUP
+    priority=150
+    nopreempt=nopreempt
+  else
+    state=BACKUP
     priority=100
+    nopreempt=preempt
   fi
 
   sed -e "s/{{state}}/$state/g" \
       -e "s/{{priority}}/$priority/g" \
-      -e "s/{{src}}/$src/g" \
+      -e "s/{{nopreempt}}/$nopreempt/g" \
       -e "s/{{peer}}/$peer/g" \
       -e "s/{{vip}}/$vip/g" \
       $KEEPALIVED_CONF.in > $KEEPALIVED_CONF
 fi
 
-exec /usr/sbin/keepalived -f $KEEPALIVED_CONF --dont-fork --log-console
+exec /usr/sbin/keepalived -f $KEEPALIVED_CONF --vrrp --dont-fork --log-console
 
